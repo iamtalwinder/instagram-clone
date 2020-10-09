9208b5960b00cfdd8a6a3eccbe019ac4de923296
Talwinder Singh <singhtalwinder790@gmail.com>
2020-10-09 16:02:29 +0530

    Added fullName & userName fields in signup

--- server/controller/user/signup.js
+++ server/controller/user/signup.js
@@ -1,9 +1,10 @@
 const bcrypt = require("bcryptjs");
 const User = require("../../model/User");
 const validation = require("./validation");
+const { getUserByUserName, getUserByEmail } = require("../../model/User");
 
 module.exports = async (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
+  const { fullName, userName, email, password } = req.body;
   const { error } = validation.signup(req.body);
   if (error) {
     return res.status(406).send({
@@ -13,20 +14,28 @@ module.exports = async (req, res) => {
   }
 
   try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    await User.createUser(req.con, firstName, lastName, email, hashedPassword);
-
-    return res.status(201).send({ msg: "User has been created." });
-  } catch (err) {
-    if (err.errno === 1062) {
-      return res.status(400).send({
-        field: "email",
-        msg: "User with the same email already exists",
+    let result = await getUserByUserName(req.con, userName);
+    if (result.length) {
+      return res.status(406).send({
+        field: "userName",
+        msg: "Username already in use.",
       });
     }
 
+    result = await getUserByEmail(req.con, email);
+    if (result.length) {
+      return res.status(406).send({
+        field: "email",
+        msg: "Email already in use.",
+      });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    await User.createUser(req.con, fullName, userName, email, hashedPassword);
+
+    return res.status(201).send({ msg: "User has been created." });
+  } catch (err) {
     console.log(err);
     return res.status(500).send({ msg: "Internal server error" });
   }
