28051b93afa6121ce78a3ec8b8f6b02e7dd1e948
Talwinder Singh <singhtalwinder790@gmail.com>
2020-10-09 11:21:00 +0530

    Added popup placeholder in FormInput

--- client/src/components/FormInput.js
+++ client/src/components/FormInput.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 import styles from "./FormInput.module.css";
 
 export default function FormInput(props) {
+  const containerId = uuidv4();
+  const inputId = uuidv4();
+  useEffect(() => {
+    const container = document.getElementById(containerId);
+    const input = document.getElementById(inputId);
+    console.log(input);
+    input.addEventListener("focus", () => {
+      container.style.borderColor = "#262626";
+    });
+
+    input.addEventListener("blur", () => {
+      container.style.borderColor = "#cfccca";
+    });
+  }, [containerId, inputId]);
+
   return (
-    <input
-      className={styles.input}
-      style={props.style}
-      name={props.name}
-      type={props.type}
-      placeholder={props.placeholder}
-      value={props.value}
-      onChange={props.onChange}
-      required={props.required}
-    />
+    <div className={styles.container} id={containerId}>
+      <input
+        id={inputId}
+        name={props.name}
+        type={props.type}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.onChange}
+        required={props.required}
+      />
+      <label>{props.placeholder}</label>
+    </div>
   );
 }
