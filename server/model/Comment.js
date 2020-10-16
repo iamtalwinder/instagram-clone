f14faea68c2cc146a6b590b857d72851fb626adf
Talwinder Singh <singhtalwinder790@gmail.com>
2020-10-16 10:09:33 +0530

    Added Comment model

---
+++ server/model/Comment.js
@@ -0,0 +1,60 @@
+module.exports = {
+  getCommentsByPostId: (con, postId) => {
+    return new Promise((resolve, reject) => {
+      con.query(
+        `
+        SELECT 
+            comment.commentId,
+            comment.userId,
+            comment.comment,
+            comment.dateAndTime,
+            user.userName
+        FROM
+            comment
+                INNER JOIN
+            user
+        WHERE
+            comment.userId = user.userId
+                AND comment.postId = "${postId}"
+        `,
+        (err, result) => {
+          if (err) reject(err);
+          else resolve(result);
+        }
+      );
+    });
+  },
+
+  createComment: (con, commentId, postId, userId, comment) => {
+    return new Promise((resolve, reject) => {
+      con.query(
+        `
+          INSERT INTO comment
+            (commentId, postId, userId, comment) 
+          VALUES 
+            ("${commentId}", "${postId}", ${userId}, "${comment}")
+          `,
+        (err, result) => {
+          if (err) reject(err);
+          else resolve(result);
+        }
+      );
+    });
+  },
+
+  deleteComment: (con, commentId) => {
+    return new Promise((resolve, reject) => {
+      con.query(
+        `
+        DELETE FROM comment
+        WHERE
+            commentId = "${commentId}"  
+        `,
+        (err, result) => {
+          if (err) reject(err);
+          else resolve(result);
+        }
+      );
+    });
+  },
+};
