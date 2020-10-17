1f41a1ec3cb72e5ed379f74c0dadfb936040f5b1
Talwinder Singh <singhtalwinder790@gmail.com>
2020-10-17 13:04:07 +0530

    Removed unnecessary comment checking in deleteComment

--- server/controller/comment/deleteComment.js
+++ server/controller/comment/deleteComment.js
@@ -4,12 +4,6 @@ module.exports = async (req, res) => {
   const userId = req.session.user.userId;
   const { commentId } = req.body;
   try {
-    const result = await Comment.getCommentByCommentId(req.con, commentId);
-
-    if (!result.length) {
-      return res.status(400).send({ msg: "No such comment exists" });
-    }
-
     await Comment.deleteComment(req.con, userId, commentId);
     return res.status(200).send({ msg: "Comment deleted" });
   } catch (err) {
