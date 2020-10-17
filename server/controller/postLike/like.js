81b4a2d8607f4445ff69e0f968b45bb48020537d
Talwinder Singh <singhtalwinder790@gmail.com>
2020-10-17 17:41:00 +0530

    Modified like & unlike controllers to support aggregate information

--- server/controller/postLike/like.js
+++ server/controller/postLike/like.js
@@ -1,4 +1,6 @@
 const PostLike = require("../../model/PostLike");
+const Utils = require("../../model/Utils");
+const PostLikes = require("../../model/PostLikes");
 
 module.exports = async (req, res) => {
   let userId = req.session.user.userId,
@@ -8,7 +10,10 @@ module.exports = async (req, res) => {
     if (result.length) {
       return res.status(400).send({ msg: "You've already liked this post" });
     }
+    await Utils.startTransaction(req.con);
     await PostLike.createLike(req.con, userId, postId);
+    await PostLikes.increment(req.con, postId);
+    await Utils.commit(req.con);
     return res.status(200).send({ msg: "Post liked" });
   } catch (err) {
     console.log(err);
