4324de9168112e11b65a3e150f83f95723bec108
Talwinder Singh <singhtalwinder790@gmail.com>
2020-10-17 12:51:39 +0530

    Modified like controller to check if a like already exists

--- server/controller/postLike/like.js
+++ server/controller/postLike/like.js
@@ -1,13 +1,12 @@
 const PostLike = require("../../model/PostLike");
-const Post = require("../../model/Post");
 
 module.exports = async (req, res) => {
   let userId = req.session.user.userId,
     { postId } = req.body;
   try {
-    const result = await Post.getPostByPostId(req.con, postId);
-    if (!result.length) {
-      return res.status(400).send({ msg: "No such post exists" });
+    const like = await PostLike.getLike(req.con, userId, postId);
+    if (result.length) {
+      return res.status(400).send({ msg: "You've already liked this post" });
     }
     await PostLike.createLike(req.con, userId, postId);
     return res.status(200).send({ msg: "Post liked" });
