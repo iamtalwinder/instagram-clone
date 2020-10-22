a99f5937490bf9eaa0db276195180fba19936ee5
Talwinder Singh <singhtalwinder790@gmail.com>
2020-10-22 08:33:25 +0530

    Added validation to getUserPosts & modified it to accept any userId

--- server/controller/post/getUserPosts.js
+++ server/controller/post/getUserPosts.js
@@ -1,11 +1,30 @@
+const Joi = require("joi");
 const Post = require("../../model/Post");
 
-module.exports = async (req, res) => {
-  const userId = req.session.user.userId;
+const validate = (data) => {
+  const schema = Joi.object({
+    userId: Joi.number().integer(),
+    refresh: Joi.boolean(),
+    start: Joi.number().integer(),
+    offset: Joi.number().integer(),
+  });
+  return schema.validate(data);
+};
 
-  const { refresh, start, offset } = req.query;
+module.exports = async (req, res) => {
+  const { error } = validate(req.query);
+  if (error) {
+    return res.status(406).send({
+      field: error.details[0].context.label,
+      msg: error.details[0].message,
+    });
+  }
 
   try {
+    const { refresh, start, offset } = req.query;
+
+    const userId = req.query.userId || req.session.user.userId;
+
     if (refresh) {
       await Post.dropTempPostTable(req.con, userId);
     }
