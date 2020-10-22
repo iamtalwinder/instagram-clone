e7bf5feff5c9e3d50bc4f9f6972cb9387490fd9c
Talwinder Singh <singhtalwinder790@gmail.com>
2020-10-22 10:17:22 +0530

    Added validation

--- server/controller/post/post.js
+++ server/controller/post/post.js
@@ -1,13 +1,32 @@
 const fs = require("fs");
+const Joi = require("joi");
 const Post = require("../../model/Post");
 const PostLikes = require("../../model/PostLikes");
 const Comments = require("../../model/Comments");
 const Utils = require("../../model/Utils");
 
+const validate = (data) => {
+  const schema = Joi.object({
+    caption: Joi.string().max(100),
+  });
+  return schema.validate(data);
+};
+
 module.exports = async (req, res) => {
   try {
     const userId = req.session.user.userId;
     const { postId, filePath } = req.file;
+
+    const { error } = validate(req.body);
+
+    if (error) {
+      fs.unlinkSync(`${filePath}.jpeg`);
+      fs.unlinkSync(`${filePath}_thumb.jpeg`);
+      return res.status(406).send({
+        field: error.details[0].context.label,
+        msg: error.details[0].message,
+      });
+    }
 
     await Utils.startTransaction(req.con);
     await Post.createPost(
@@ -24,8 +43,14 @@ module.exports = async (req, res) => {
     res.status(201).send({ postId: postId, msg: "Uploaded" });
   } catch (err) {
     console.log(err);
-    fs.unlink(`${filePath}.jpeg`);
-    fs.unlink(`${filePath}_thumb.jpeg`);
+
+    fs.unlink(`${filePath}.jpeg`, (err) => {
+      console.log(err);
+    });
+
+    fs.unlink(`${filePath}_thumb.jpeg`, (err) => {
+      console.log(err);
+    });
     return res.status(500).send({ msg: "Internal server error" });
   }
 };
