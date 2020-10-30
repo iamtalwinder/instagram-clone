f82a2c5b0948745705eeb4365f918d77ee70c846
Talwinder Singh <singhtalwinder790@gmail.com>
2020-10-30 07:30:39 +0530

    Added toast component

---
+++ client/src/components/toast.js
@@ -0,0 +1,24 @@
+import { Notyf } from "notyf";
+import "notyf/notyf.min.css";
+
+export default new Notyf({
+  duration: 2500,
+  position: {
+    x: "center",
+    y: "bottom",
+  },
+  types: [
+    {
+      type: "info",
+      background: "black",
+      color: "white",
+      icon: false,
+    },
+    {
+      type: "error",
+      background: "red",
+      color: "white",
+      icon: false,
+    },
+  ],
+});
