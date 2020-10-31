4aa49e086bc119485bfa3af512cd93d8739017c7
Talwinder Singh <singhtalwinder790@gmail.com>
2020-10-31 11:52:46 +0530

    Added usePersistedState hook

--- client/src/context/LoggedInUser.js
+++ client/src/context/LoggedInUser.js
@@ -1,26 +1,10 @@
-import React, { useState, useEffect, createContext } from "react";
+import React, { createContext } from "react";
+import usePersistedState from "../hooks/usePersistedState";
 
 export const LoggedInUserContext = createContext({});
 
 export const LoggedInUserContextProvider = (props) => {
-  const [loggedInUser, setLoggedInUser] = useState({});
-
-  useEffect(() => {
-    try {
-      const data = JSON.parse(localStorage.getItem("loggedInUser"));
-      if (data) {
-        setLoggedInUser(data);
-      } else {
-        setLoggedInUser({});
-      }
-    } catch (err) {
-      setLoggedInUser({});
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("loggedInUser", JSON.stringify(loggedInUser));
-  });
+  const [loggedInUser, setLoggedInUser] = usePersistedState("loggedInUser", {});
 
   return (
     <LoggedInUserContext.Provider value={[loggedInUser, setLoggedInUser]}>
---
+++ client/src/hooks/usePersistedState.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export default function usePersistedState(key, init) {
+  const getSavedValue = (key, init) => {
+    const savedValue = JSON.parse(localStorage.getItem(key));
+
+    if (savedValue) {
+      return savedValue;
+    }
+
+    if (init instanceof Function) {
+      return init();
+    }
+
+    return init;
+  };
+
+  const [state, setState] = useState(() => {
+    return getSavedValue(key, init);
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state]);
+
+  return [state, setState];
+}
