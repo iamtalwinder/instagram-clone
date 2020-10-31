a9baad208e29c39cb5c405c453c3b8fdd21d03ca
Talwinder Singh <singhtalwinder790@gmail.com>
2020-10-31 11:10:38 +0530

    Removed Id

--- client/src/components/TextButton.js
+++ client/src/components/TextButton.js
@@ -9,7 +9,6 @@ export default function Button(props) {
   }
   return (
     <button
-      id={props.id}
       className={props.disabled ? styles.disabled : styles.active}
       style={props.style}
       type={props.type}
