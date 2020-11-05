d48528c8e1bd0f1c8316a02129f1d54eb0a5e11e
Talwinder Singh <singhtalwinder790@gmail.com>
2020-11-05 14:15:47 +0530

    Added text in ToAccount

--- client/src/components/ToAccount.js
+++ client/src/components/ToAccount.js
@@ -8,20 +8,25 @@ export default function ToAccount(props) {
   const history = useHistory();
 
   return (
-    <EmptyButton
-      style={{ display: "flex" }}
-      onClick={() => {
-        history.push({
-          pathname: "/account",
-          state: { userId: props.userId },
-        });
-      }}
-    >
-      <DpThumb
-        style={{ height: "25px", width: "25px" }}
-        dpPath={props.dpPath}
-      />
-      <p className={styles.username}>{props.username}</p>
-    </EmptyButton>
+    <div className={styles.container}>
+      <EmptyButton
+        style={{ display: "flex" }}
+        onClick={() => {
+          history.push({
+            pathname: "/account",
+            state: { userId: props.userId },
+          });
+        }}
+      >
+        {props.dpPath && (
+          <DpThumb
+            style={{ height: "25px", width: "25px", marginRight: "10px" }}
+            dpPath={props.dpPath}
+          />
+        )}
+        <p className={styles.username}>{props.username}</p>
+      </EmptyButton>
+      <p className={styles.text}>{props.text}</p>
+    </div>
   );
 }
