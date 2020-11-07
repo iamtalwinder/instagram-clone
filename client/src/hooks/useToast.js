508d5a3b4fd4d0145fc140200397feb805941372
Talwinder Singh <singhtalwinder790@gmail.com>
2020-11-07 10:56:26 +0530

    Wrapped toast object in useRef to avoid re-renders

--- client/src/hooks/useToast.js
+++ client/src/hooks/useToast.js
@@ -1,26 +1,29 @@
+import { useRef } from "react";
 import { Notyf } from "notyf";
 import "notyf/notyf.min.css";
 
 export default function useToast() {
-  return new Notyf({
-    duration: 2500,
-    position: {
-      x: "center",
-      y: "bottom",
-    },
-    types: [
-      {
-        type: "info",
-        background: "black",
-        color: "white",
-        icon: false,
+  return useRef(
+    new Notyf({
+      duration: 2500,
+      position: {
+        x: "center",
+        y: "bottom",
       },
-      {
-        type: "error",
-        background: "red",
-        color: "white",
-        icon: false,
-      },
-    ],
-  });
+      types: [
+        {
+          type: "info",
+          background: "black",
+          color: "white",
+          icon: false,
+        },
+        {
+          type: "error",
+          background: "red",
+          color: "white",
+          icon: false,
+        },
+      ],
+    })
+  ).current;
 }
