93f01527f3bad4a2d9447f188ca9c9652f5ebd1d
Talwinder Singh <singhtalwinder790@gmail.com>
2020-11-17 13:25:51 +0530

    Added ellipsis to overflowing username

--- client/src/components/UnfollowModal.js
+++ client/src/components/UnfollowModal.js
@@ -48,7 +48,7 @@ export default function UnfollowModal({
             margin: "auto",
           }}
         />
-        <p>unfollow @{username}</p>
+        <p className={styles.username}>unfollow @{username}</p>
       </div>
 
       <Button style={{ color: "red" }} onClick={unfollow}>
